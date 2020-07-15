aa35c2006332d9a9e4739d46be13819ed43314b1
20chan <2@0chan.dev>
2020-07-15 17:10:12 +0900

    proper way to get title and extra info

--- src/background/background.ts
+++ src/background/background.ts
@@ -6,7 +6,11 @@ chrome.runtime.onInstalled.addListener(d => {
         chrome.tabs.query({audible: true}, tabs => {
             tabs.forEach(t => {
                 if (t.audible && t.url.startsWith('https://music.youtube.com/')) {
-                    console.log(t.title);
+                    chrome.tabs.executeScript(t.id, {
+                        code: "[document.querySelector('yt-formatted-string.ytmusic-player-bar.title').innerText, document.querySelector('yt-formatted-string.ytmusic-player-bar.byline').title]"
+                    }, results => {
+                        console.log(`${results[0]}\t${results[1]}`);
+                    });
                 }
             });
         });
