1823501a8f51994b86eb9bd7cd04dd515d5b372f
20chan <2@0chan.dev>
2020-07-19 13:53:26 +0900

    Now id and oauth key are read from option storage

--- src/background/background.ts
+++ src/background/background.ts
@@ -12,7 +12,6 @@ interface PlayingInfo {
     updatedAt: Date;
 }
 
-chrome.alarms.create('refresh', { periodInMinutes: 1 });
 chrome.alarms.onAlarm.addListener(a => {
     if (a.name !== 'refresh') return;
     report();
@@ -23,8 +22,16 @@ chrome.browserAction.onClicked.addListener(tab => {
 });
 
 chrome.runtime.onMessage.addListener((message, sender) => {
-    if (message.data === "manualReport") {
+    if (message.data === 'manualReport') {
         report();
+    }
+
+    if (message.data === 'enabled') {
+        chrome.alarms.create('refresh', { periodInMinutes: 1 });
+        report();
+    }
+    if (message.data === 'disabled') {
+        chrome.alarms.clear('refresh');
     }
 });
 
@@ -127,53 +134,54 @@ const report = () => {
 }
 
 const update = (info: PlayingInfo) => {
-    const id = "";
-    const oauth = "";
-    const message = "auto update by github-now";
-
-    const auth = `token ${oauth}`;
-    fetch(`https://api.github.com/repos/${id}/${id}/contents/README.template.md`, {
-        headers: {
-            "Authorization": auth
-        },
-    }).then(async resp => {
-        const json = await resp.json();
-        const template = decodeURIComponent(escape(atob(json.content)));
-        // looks like need a template engine..
-        const content = template
-        .replace("{CURRENT_PLAYING_SOURCE}", info.src)
-        .replace("{CURRENT_PLAYING_NAME}", info.name)
-        .replace("{CURRENT_PLAYING_ARTISTS}", info.artists)
-        .replace("{CURRENT_PLAYING_ALBUM}", info.album)
-        .replace("{CURRENT_PLAYING_RELEASED}", info.year)
-        .replace("{CURRENT_PLAYING_ALBUM_SRC}", info.albumCoverImage)
-        .replace("{CURRENT_PLAYING_URL}", info.url)
-        .replace("{CURRENT_PLAYING_LAST_UPDATED}", format(info.updatedAt, 'MM/DD/YYYY HH:mm'));
-        const encoded = btoa(unescape(encodeURIComponent(content)));
-        const target = await fetch(`https://api.github.com/repos/${id}/${id}/contents/README.md`);
-        const sha = (await target.json()).sha;
-        try {
-            const postResp = await fetch(`https://api.github.com/repos/${id}/${id}/contents/README.md`, {
-                method: "PUT",
-                headers: {
-                    "Authorization": auth
-                },
-                body: JSON.stringify({
-                    "message": message,
-                    "content": encoded,
-                    "sha": sha,
-                    "committer": {
-                        "name": "github-now",
-                        "email": "2+github-now@0chan.dev",
+    chrome.storage.local.get(['id', 'oauth'], items => {
+        const id = items.id as string;
+        const oauth = items.key as string;
+        const message = 'auto update by github-now';
+        const auth = `token ${oauth}`;
+        fetch(`https://api.github.com/repos/${id}/${id}/contents/README.template.md`, {
+            headers: {
+                "Authorization": auth
+            },
+        }).then(async resp => {
+            const json = await resp.json();
+            const template = decodeURIComponent(escape(atob(json.content)));
+            // looks like need a template engine..
+            const content = template
+            .replace("{CURRENT_PLAYING_SOURCE}", info.src)
+            .replace("{CURRENT_PLAYING_NAME}", info.name)
+            .replace("{CURRENT_PLAYING_ARTISTS}", info.artists)
+            .replace("{CURRENT_PLAYING_ALBUM}", info.album)
+            .replace("{CURRENT_PLAYING_RELEASED}", info.year)
+            .replace("{CURRENT_PLAYING_ALBUM_SRC}", info.albumCoverImage)
+            .replace("{CURRENT_PLAYING_URL}", info.url)
+            .replace("{CURRENT_PLAYING_LAST_UPDATED}", format(info.updatedAt, 'MM/DD/YYYY HH:mm'));
+            const encoded = btoa(unescape(encodeURIComponent(content)));
+            const target = await fetch(`https://api.github.com/repos/${id}/${id}/contents/README.md`);
+            const sha = (await target.json()).sha;
+            try {
+                const postResp = await fetch(`https://api.github.com/repos/${id}/${id}/contents/README.md`, {
+                    method: "PUT",
+                    headers: {
+                        "Authorization": auth
                     },
-                }),
-            });
+                    body: JSON.stringify({
+                        "message": message,
+                        "content": encoded,
+                        "sha": sha,
+                        "committer": {
+                            "name": "github-now",
+                            "email": "2+github-now@0chan.dev",
+                        },
+                    }),
+                });
 
-            if (postResp.ok) {
-                console.log("updated");
+                if (postResp.ok) {
+                    console.log("updated");
+                }
+            } catch (postResp) {
+                console.log(`Error ${postResp}`);
             }
-        } catch (postResp) {
-            console.log(`Error ${postResp}`);
-        }
-    })
+        });
+    });
 }
